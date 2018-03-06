880f9bca0bff4c8294afd960f16eeea0ab81db34
Omar Ihmoda <omarihmoda@Omars-MacBook-Pro.local>
2018-03-06 11:02:07 -0800

    login initial commit

--- server/models/user.js
+++ server/models/user.js
@@ -1,21 +1,26 @@
 //////////////////////////////////////////////////////////////////////////
-//  SERVER/MODELS/MODEL_NAME_SINGULAR.JS FILE [ SCHEMA / MODEL] 
+//  SERVER/MODELS/MODEL_NAME_SINGULAR.JS FILE [ SCHEMA / MODEL]
 //////////////////////////////////////////////////////////////////////////
 // Require Mongoose module (STEP 1) in the following files:
-// - mongoose.js file, 
-// - CONTROLLER file, 
+// - mongoose.js file,
+// - CONTROLLER file,
 // - HERE.
 //////////////////////////////////////////////////////////////////////////
 
 // STEP 1 (DB/SCHEMA SETUP):
 // Require Mongoose module:
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 // STEP 3 (DB/SCHEMA SETUP):
 // Name & Describe Schema:
-var modelName = new mongoose.Schema({  //<--- SET MODEL NAME & DESCRIBE MODEL'S SCHEMA
-    model_attribute: Number
-})
+var UserSchema = new mongoose.Schema(
+  {
+    //<--- SET MODEL NAME & DESCRIBE MODEL'S SCHEMA
+    email: { type: String, required: true },
+    password: { type: String, required: true }
+  },
+  { timestamps: true }
+);
 
 // Set this Schema in our Models as 'Schema_Instance':
-mongoose.model('model_instance', modelName);    //<-- NAME YOUR MODEL INSTANCE
+var User = mongoose.model("User", UserSchema); //<-- NAME YOUR MODEL INSTANCE
